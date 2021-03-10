7789b384aca5e0ae73da0ec0a2bfea1bb9b69612
unknown <lucas46em@gmail.com>
2021-03-10 17:09:17 -0300

    Day 2 - typeorm, models, controllers, router

---
+++ backend/src/database/migrations/1615391132107-CreateUsers.ts
@@ -0,0 +1,38 @@
+import {MigrationInterface, QueryRunner, QueryRunnerAlreadyReleasedError, Table} from "typeorm";
+
+export class CreateUsers1615391132107 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(
+            new Table({
+                name:"users",
+                columns:[
+                    {
+                        name:"id",
+                        type:'uuid',
+                        isPrimary: true
+                    },
+                    {
+                        name:'name',
+                        type:'varchar'
+                    },
+                    {
+                        name:'email',
+                        type:'varchar'
+                    },
+                    {
+                        name:'created_at',
+                        type:'timestamp',
+                        default:"now()"
+                    }
+
+                ]
+            })
+        )
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropTable("users");
+    }
+
+} 
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,13 +1,11 @@
+import 'reflect-metadata';
 import express, { response } from 'express';
+import './database';
+import { router } from './routes';
 
 const app = express();
- 
-app.get('/users',(request,response)=>{
-    return response.json({"message":"hello word"})
-})
 
-app.post("/users",(request,response)=>{
-    return response.json({message:"Dados salvos"})
-})
+app.use(express.json())
+app.use(router)
 
 app.listen(3333,()=>console.log('server is running'));
