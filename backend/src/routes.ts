a80981cf184f52d0fe459edf71f4d3e0ab5fd921
unknown <lucas46em@gmail.com>
2021-03-11 16:58:52 -0300

    Day 3 - repository pattern, go on

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,9 +1,13 @@
 import { Router } from 'express';
+import { SurveysController } from './controllers/SurveysController';
 import { UserController } from './controllers/UserController';
 
 const router = Router();
 const userController = new UserController;
+const surveyController = new SurveysController;
 
 router.post('/users',userController.create )
+router.get('/surveys',surveyController.index )
+router.post('/surveys',surveyController.create )
 
 export {router}
