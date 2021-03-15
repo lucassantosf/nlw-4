cfa8b1f05012d6ccf9c137c60a50384965b48227
unknown <lucas46em@gmail.com>
2021-03-15 15:17:42 -0300

    Day 4 - sending mail with nodemailer, template mail handlebars

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,13 +1,17 @@
 import { Router } from 'express';
+import { SendMailController } from './controllers/SendMailController';
 import { SurveysController } from './controllers/SurveysController';
 import { UserController } from './controllers/UserController';
 
 const router = Router();
 const userController = new UserController;
 const surveyController = new SurveysController;
+const sendMailController = new SendMailController;
 
 router.post('/users',userController.create )
 router.get('/surveys',surveyController.index )
 router.post('/surveys',surveyController.create )
+
+router.post('/sendMail',sendMailController.execute)
 
 export {router}
